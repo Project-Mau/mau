352231da845aaea8a63fd3398561b5ee271adf1c
Leonardo Giordani <giordani.leonardo@gmail.com>
2024-03-29 15:36:37 +0000

    Content nodes migrated to new node structure

--- mau/nodes/content.py
+++ mau/nodes/content.py
@@ -1,7 +1,7 @@
-from mau.nodes.page import PageNode
+from mau.nodes.page import SupaNode
 
 
-class ContentNode(PageNode):
+class ContentNode(SupaNode):
     """Content included in the page.
 
     This represents generic content included in the page.
@@ -22,33 +22,36 @@ class ContentNode(PageNode):
         uri_args=None,
         uri_kwargs=None,
         title=None,
+        parent=None,
+        children=None,
         subtype=None,
         args=None,
         kwargs=None,
         tags=None,
     ):
-        super().__init__(subtype, args, kwargs, tags)
-        self.uri_args = uri_args or []
-        self.uri_kwargs = uri_kwargs or {}
+        super().__init__(
+            parent=parent,
+            children=children,
+            subtype=subtype,
+            args=args,
+            kwargs=kwargs,
+            tags=tags,
+        )
         self.content_type = content_type
         self.title = title
+        self.uri_args = uri_args
+        self.uri_kwargs = uri_kwargs
 
-    @property
-    def _content(self):
+    def _custom_dict(self):
         return {
-            "type": self.node_type,
             "content_type": self.content_type,
+            "title": self.title,
             "uri_args": self.uri_args,
             "uri_kwargs": self.uri_kwargs,
-            "title": self.title,
-            "subtype": self.subtype,
-            "args": self.args,
-            "kwargs": self.kwargs,
-            "tags": self.tags,
         }
 
 
-class ContentImageNode(PageNode):
+class ContentImageNode(SupaNode):
     """An image included in the page."""
 
     node_type = "content_image"
@@ -59,27 +62,30 @@ class ContentImageNode(PageNode):
         alt_text=None,
         classes=None,
         title=None,
+        parent=None,
+        children=None,
         subtype=None,
         args=None,
         kwargs=None,
         tags=None,
     ):
-        super().__init__(subtype, args, kwargs, tags)
+        super().__init__(
+            parent=parent,
+            children=children,
+            subtype=subtype,
+            args=args,
+            kwargs=kwargs,
+            tags=tags,
+        )
         self.uri = uri
-        self.classes = classes
         self.title = title
         self.alt_text = alt_text
+        self.classes = classes
 
-    @property
-    def _content(self):
+    def _custom_dict(self):
         return {
-            "type": self.node_type,
             "uri": self.uri,
+            "title": self.title,
             "alt_text": self.alt_text,
             "classes": self.classes,
-            "title": self.title,
-            "subtype": self.subtype,
-            "args": self.args,
-            "kwargs": self.kwargs,
-            "tags": self.tags,
         }
